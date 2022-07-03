a7898e2f8e3da7e263843277dc577601bfc64a5e
Oliver Xie <oliverxie19@gmail.com>
2022-07-02 18:00:39 -0700

    feat: add sans-serif heading, serif body, and stack layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("normalize.css");
+    eleventyConfig.addPassthroughCopy("frosh2022.css");
 
     return {
         passthroughFileCopy: true
