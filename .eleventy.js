aff7871f308a032b94da52f9b6ebcedc1b34611c
Oliver Xie <oliverxie19@gmail.com>
2022-07-20 23:53:19 -0700

    feat: add navigation header - hamburger menu + dropdown on narrow screen - active section highlight + scroll

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("normalize.css");
     eleventyConfig.addPassthroughCopy("frosh2022.css");
+    eleventyConfig.addPassthroughCopy("bootstrap.min-scrollspy-collapse-transitions.js");
 
     return {
         passthroughFileCopy: true
