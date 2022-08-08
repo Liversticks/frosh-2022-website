3bf4b7acd1eaf6606d1258c251df3c27082fa5b0
Oliver Xie <oliverxie19@gmail.com>
2022-08-07 23:46:42 -0700

    - add favicon assets (not wired up in this repo) - add main and sponsor logos - adjusted colour scheme - replaced all lipsum with event details

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("normalize.css");
     eleventyConfig.addPassthroughCopy("frosh2022.css");
     eleventyConfig.addPassthroughCopy("bootstrap.min-scrollspy-collapse-transitions.js");
+    eleventyConfig.addPassthroughCopy("sponsors");
+    eleventyConfig.addPassthroughCopy("favicon");
+    eleventyConfig.addPassthroughCopy("Frosh2022_Logo_NoSH_DarkSubtitle.png");
 
     return {
         passthroughFileCopy: true
